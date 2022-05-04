994971053dadae774ed5364c02e24d8ad4b8a1cf
dariobossy <dario.bossy@stud.hslu.ch>
2022-05-04 11:17:55 +0200

    sprint 4

---
+++ sprint4/script.js
@@ -0,0 +1,34 @@
+let divs = document.querySelectorAll(".wrapper")
+
+var letters = ["T","Y","P","O","G","R","A","P","H","I","E"]
+
+for(let i = 0; i < letters.length; i++) {
+    divs[i].addEventListener("click", function() {
+        shuffle(letters);
+        for(let i = 0; i < letters.length; i++) {
+            divs[i].innerHTML = "<span>" + letters[i] + "</span>";
+        }
+    });
+}
+
+// Array SHuffle
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
